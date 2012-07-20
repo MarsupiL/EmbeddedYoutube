19ded36b0fa9bba1bf1fcb9200acad945fb2b6ae
Brandon Salazar <aneffingonion@Brandons-MacBook-Pro.local>
2012-07-20 18:36:56 -0400

    Initial Commit

---
+++ EmbeddedYoutubeVideoTests/EmbeddedYoutubeVideoTests.h
@@ -0,0 +1,13 @@
+//
+//  EmbeddedYoutubeVideoTests.h
+//  EmbeddedYoutubeVideoTests
+//
+//  Created by Brandon Salazar on 7/20/12.
+//  Copyright (c) 2012 USI. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EmbeddedYoutubeVideoTests : SenTestCase
+
+@end
---
+++ EmbeddedYoutubeVideoTests/EmbeddedYoutubeVideoTests.m
@@ -0,0 +1,32 @@
+//
+//  EmbeddedYoutubeVideoTests.m
+//  EmbeddedYoutubeVideoTests
+//
+//  Created by Brandon Salazar on 7/20/12.
+//  Copyright (c) 2012 USI. All rights reserved.
+//
+
+#import "EmbeddedYoutubeVideoTests.h"
+
+@implementation EmbeddedYoutubeVideoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EmbeddedYoutubeVideoTests");
+}
+
+@end
